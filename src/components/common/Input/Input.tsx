3e5a896a0c7c63077a2d00439098db31cc668369
jeff0518 <cycivs07@icloud.com>
2025-02-15 20:57:44 +0800

    fix(Input): fix input css

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,15 +1,15 @@
 import { InputProps } from "./Input.interface";
 
-const baseStyle = `w-full p-1 pl-10 pr-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-vermilion-500`;
+const baseStyle = `w-full p-1 pr-3 focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-vermilion-500`;
 
 const Input = (props: InputProps) => {
   const { type, id, placeholder, icon: Icon } = props;
   return (
-    <div>
-      <label htmlFor={id} className="opacity-0">
+    <div className="flex items-center gap-2">
+      <Icon className="text-gray-500" />
+      <label htmlFor={id} className="sr-only">
         {placeholder}
       </label>
-      <Icon className="" />
       <input
         type={type}
         className={baseStyle}
